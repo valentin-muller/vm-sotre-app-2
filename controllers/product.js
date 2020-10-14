b63e9b8c291f4cc76c675220a4feb01237a88599
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-10-14 11:09:01 +0200

    Create New Product

--- controllers/product.js
+++ controllers/product.js
@@ -1,32 +1,73 @@
-const formidable = require('formidable') // Or can use Multer
-const _ = require('lodash')
-const fs = require('fs')
+const formidable = require("formidable"); // Or can use Multer
+const _ = require("lodash");
+const fs = require("fs");
 const Product = require("../models/product");
-const { errorHandler } = require('../helpers/dbErrorHandler');
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "Product not found",
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    res.json(req.product);
+}
 
 exports.create = (req, res) => {
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true
-    form.parse(req, (err, fields, files) => {
-        if(err) {
-            return res.status(400).json({
-                error: 'Image could not be uplodaed'
-            })
-        }
-        let product = new Product(fields)
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uplodaed",
+      });
+    }
 
-        if (files.photo) {
-            product.photo.data = fs.readFileSync(files.photo.path)
-            product.photo.contentType = files.photo.type
-        }
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
 
-        product.save((err, result) => {
-            if (err) {
-                return res.status(400).json({
-                    error: errorHandler(error)
-                })
-            }
-            res.json(result);
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
+    let product = new Product(fields);
+
+    if (files.photo) {
+      // console.log('FILES PHOTO', files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1MB",
         });
+      }
+
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(error),
+        });
+      }
+      res.json(result);
     });
+  });
 };
