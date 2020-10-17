5833acd866b7e1717bce3097b2131aefbd5954a9
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-10-17 13:09:27 +0200

    User get and update

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { userById } = require("../controllers/user");
+const { userById, read, update } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({
@@ -11,6 +11,10 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
     });
 });
 
+router.get("/user/:userId", requireSignin, isAuth, read)
+
+
+router.put("/user/:userId", requireSignin, isAuth, update)
 router.param("userId", userById);
 
 module.exports = router;
